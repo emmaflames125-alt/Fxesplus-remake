bd031d041bfbe214dd46b07d385342a66033c5f6
emmaflames125-alt <emmaflames125@gmail.com>
2026-03-27 21:53:29 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = "chimex-app-v1";
+const CACHE_NAME = 'rop-compiler-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
 
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache=>{
-            return cache.addAll([
-                "./",
-                "./index.html"
-            ]);
-        })
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener("fetch", e=>{
-    e.respondWith(
-        caches.match(e.request).then(res=>{
-            return res || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
